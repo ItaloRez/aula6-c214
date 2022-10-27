4a1aa3b837512ad6c7c2889bfd32763d17061e93
Ítalo de Rezende <italoeji@hotmail.com>
2022-10-27 04:52:49 -0300

    exer 11

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import app from "./app.js";
-import db from "./database/index.js";
-
-db.connect();
-app.listen(4000);
--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import carRouter from "./car.js";
-
-const router = Router();
-
-router.use(carRouter);
-
-export default router;
